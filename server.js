3b0c49ce6f6d15d8a358267f534dc5bcb23d4f94
unknown <guilhermefaria0295@digitalcollege.internal>
2026-04-11 08:53:32 -0300

    Atualização do server.js

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Requisição chegou!');
-})
+    res.send("Requisição recebida!")
+});
 
 app.get('/home', (req, res) => {
-    res.send('Home Page!');
-})
+    res.send("Home Page");
+});
+
+/**
+ * Desafio:
+ * Obter o mesmo resulta porem no lugar de usar a url com query
+ * http://localhost:1234/me?cidade=Natal&nome=Jeselito
+ * 
+ * Usar a url com parametros dinamicos
+ * http://localhost:1234/me/Jeselito/Maracanau
+ * 
+ * Joselito e Maracanau podem ser qualquer coisa, são informações
+ * passadas na url assim como a query.
+ * 
+ * Documentação do express sobre rotas
+ * https://expressjs.com/pt-br/guide/routing.html
+ */
 app.get('/me', (req, res) => {
-    res.send('Meu nome é Guilherme, moro em Fortaleza!');
-})
+    const {nome, cidade} = req.query;
+    res.send(`Meu nome é ${nome}, Moro em ${cidade}`);
+});
 
 app.listen(1234, () => {
     console.log("Servidor iniciado na porta 1234");
