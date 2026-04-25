7c9b9058ed16758ae58e35f90cba36256481ae19
unknown <guilhermefaria0295@digitalcollege.internal>
2026-04-25 11:37:15 -0300

    criando Update e o FindUserById

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -14,28 +14,56 @@
  */
 
 const fakeUsers = [
-    {name: "Joselito", email: 'joselito@mail.com'}
+    {id: 1, name: "Joselito", email: 'joselito@mail.com'}
 ];
 
-async function ListUser(req, res) {
-    res.status(200)
-    const response = await fetch('https://http.dog/200.jpg');
-    const imagem = await response.text()
-    const headers = new Map;
-    headers.set('Content-type', "image/jpg")
-    res.setHeaders(headers);
-    res.send(Buffer.from(imagem));
+function ListUser(req, res) {
+    res.json(fakeUsers);
+}
+
+function FindUserById(req, res) {
+    const {id} = req.params;
+    const user = fakeUsers.find(user => Number(user.id) === Number(id));
+
+    if(!user){
+        res.status(404);
+        return res.json({
+            error: "Not Found !!!"
+        });
+    }
+    res.json(user);
 }
 
 function CreateUser(req, res) {
     const {name, email} = req.body;
-    fakeUsers.push({name, email});
+
+    const id = fakeUsers.length + 1;
+
+    fakeUsers.push({id, name, email});
     res.json({
         message: "Usuario cadastrado com sucesso!"
     });
 }
 
+function UpdateUser(req, res) {
+    const {id} = req.params;
+    const {name, email} = req.body;
+
+    fakeUsers.forEach(user => {
+        if(Number(user.id) === Number(id)) {
+            user.name = name || user.name;
+            user.email = email || user.email;
+        }
+    });
+
+    res.send({
+        message: 'Usuário encontrado com sucesso!'
+    });
+}
+
 module.exports = {
     ListUser,
-    CreateUser
+    FindUserById,
+    CreateUser,
+    UpdateUser
 }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const { ListUser, CreateUser } = require("../controllers/User");
+const { ListUser, CreateUser, UpdateUser, FindUserById } = require("../controllers/User");
 
 const route = Router();
 
-route.get("/users", ListUser);
-route.post("/users", CreateUser);
+route.get("/users/", ListUser);
+route.get("/users/:id", FindUserById);
+route.post("/users/", CreateUser);
+route.put("/users/:id", UpdateUser);
 
 module.exports = route;
